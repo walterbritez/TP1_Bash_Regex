ee3dceba8f815f4f3046905520c8fee616e0bdaf
Walter <wbritez@fi.uba.ar>
2025-04-07 10:28:51 -0300

    Se finaliza el ejercicio0

---
+++ ejercicio0.sh
@@ -0,0 +1,11 @@
+sumar_energia(){
+    n1="$1"
+    n2="$2"
+    suma=$((n1+n2))
+    echo $suma
+}
+
+energia_antigua="$1"
+energia_adicional="$2"
+
+echo $(sumar_energia "$energia_antigua" "$energia_adicional")
